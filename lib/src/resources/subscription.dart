091eb9e3b74a39422a7c51e18ac6504d214a1bbf
Yurii Prykhodko <yurii.prykhodko+github.us@solid.software>
2024-02-22 17:28:45 +0200

    Add DELETE subscriptions/:id endpoint

--- lib/src/resources/subscription.dart
+++ lib/src/resources/subscription.dart
@@ -19,4 +19,21 @@ class SubscriptionResource extends Resource<Subscription> {
     return DataList<Subscription>.fromJson(
         map, (value) => Subscription.fromJson(value as Map<String, dynamic>));
   }
+
+  /// https://docs.stripe.com/api/subscriptions/cancel
+  Future<Subscription> cancel(
+    String id, {
+    bool? invoiceNow,
+    bool? prorate,
+  }) async {
+    final response = await delete(
+      'subscription/$id',
+      data: {
+        if (invoiceNow != null) 'invoice_now': invoiceNow,
+        if (prorate != null) 'prorate': invoiceNow,
+      },
+    );
+
+    return Subscription.fromJson(response);
+  }
 }
