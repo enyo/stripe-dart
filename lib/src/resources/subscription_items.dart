932cd122cd830a806faf10dd7aa22c31ca8c9fcc
Yurii Prykhodko <yurii.prykhodko+github.us@solid.software>
2024-03-04 18:59:43 +0200

    Add SubscriptionItems resource

---
+++ lib/src/resources/subscription_items.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:stripe/messages.dart';
+
+import '../client.dart';
+import '_resource.dart';
+
+class SubscriptionItemResource extends Resource<SubscriptionItem> {
+  SubscriptionItemResource(Client client) : super(client);
+
+  Future<SubscriptionItem> retrieve(String id) async {
+    final response = await get('subscription_items/$id');
+    return SubscriptionItem.fromJson(response);
+  }
+
+  Future<DataList<SubscriptionItem>> list(
+      [ListSubscriptionsRequest? request]) async {
+    final map =
+        await get('subscription_items', queryParameters: request?.toJson());
+    return DataList<SubscriptionItem>.fromJson(
+      map,
+      (value) => SubscriptionItem.fromJson(value as Map<String, dynamic>),
+    );
+  }
+
+  Future<SubscriptionItem> update(
+    String id, {
+    required SubscriptionUpdate update,
+  }) async {
+    final response = await post(
+      'subscription_items/$id',
+      data: update.toJson(),
+    );
+
+    return SubscriptionItem.fromJson(response);
+  }
+
+  Future<SubscriptionItem> deleteItem(
+    String id, {
+    bool? clearUsage,
+  }) async {
+    final response = await delete('subscription_items/$id');
+
+    return SubscriptionItem.fromJson(response);
+  }
+}
